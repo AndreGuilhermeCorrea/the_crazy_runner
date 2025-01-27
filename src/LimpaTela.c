cc7b5eaf2edcf3a40d00772403945083ab60584f
AndreGuilhermeCorrea <de_correa@msn.com>
2025-01-27 12:54:05 -0300

    🔧 chore: Clean Screen

--- src/LimpaTela.c
+++ src/LimpaTela.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include "Header.h"
 
 void limpaTela() {
     system("cls");
+    telaXY(2, 25);
+    printf("PRESSIONE X PARA VOLTAR AO MENU"); 
 }
 
 //Fim do código
