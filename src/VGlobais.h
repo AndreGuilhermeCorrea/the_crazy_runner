ecb39ac6fa97d1f4f17df584589e0a63ec48253d
AndreGuilhermeCorrea <de_correa@msn.com>
2025-01-27 12:59:11 -0300

    🔧 chore: Global Variables

--- src/VGlobais.h
+++ src/VGlobais.h
@@ -1,6 +1,15 @@
 #ifndef VGLOBAIS_H
 #define VGLOBAIS_H
 
+//Definição de constantes do jogo com uso de struct para facilitar a manipulação
+typedef struct
+{
+    int score;
+    char nome[30];
+} Player;
+
+#define T_INICIAL 0 // Tempo inicial do jogo
+
 #endif
 
 //Fim do código
