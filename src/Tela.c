cbdf57b382539398de3abd02588a05cf30017eaf
AndreGuilhermeCorrea <de_correa@msn.com>
2025-01-27 12:39:16 -0300

    🔧 chore: Coordinate Screen

--- src/Tela.c
+++ src/Tela.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
+// // Função para posicionar o cursor na tela
 void telaXY() {
-
-
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord); // Posiciona o cursor na tela
 }
 
 //Fim do código
