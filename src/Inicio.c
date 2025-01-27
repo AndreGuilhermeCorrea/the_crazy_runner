772970c0f1b55e11714f46d1a051519688488ef7
AndreGuilhermeCorrea <de_correa@msn.com>
2025-01-27 13:16:31 -0300

    🔧 chore: Screen Game Init

--- src/Inicio.c
+++ src/Inicio.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h> 
+#include <time.h> 
+#include <windows.h> 
+
+#include "Header.h"
+#include "VGlobais.h"
 
 void iniciar() {
+    system("cls"); 
+
+    telaXY(5, 2); 
+    printf("Aperte X para sair, Aperte W para Pular"); 
+    telaXY(48, 2); 
+    printf("SCORE : "); 
     
+    // Exibe o chão
+    telaXY(1, 25); 
+    for (int x = 0; x < 82; x++) 
+    {
+        printf("__"); // Exibe caracteres "_" na tela
+    }
 }
 
 //Fim do código
